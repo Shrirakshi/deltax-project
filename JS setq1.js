28197947c995fec892f7cc8cdc3a59855d9c2ce5
Shrirakshi K.L <89006805+Shrirakshi@users.noreply.github.com>
2023-01-27 22:36:29 +0530

    Add files via upload

---
+++ JS setq1.js
@@ -0,0 +1,35 @@
+for(let i=0;i<=s2.length;i+=1){
+arr[i][0]=0;
+}
+  let len=0;
+  let col=0;
+  let row=0;
+  for(let i=0;i<=s2.length;i+=1){
+    for(let j=1;j<=s1.length;j+=1){
+    if(s1[j-1]==s2[i-1])
+    {
+      arr[i][j]=arr[i-1][j-1]+1;
+    }
+   else{
+    arr[i][j]=0;
+ }
+ if(arr[i][j]>len){
+ len=arr[i][j];
+  col=j;
+  row=i;
+  }
+ }
+}
+if(len==0)
+{
+ return ' ';
+}
+let res=' ';
+while(arr[row][col]>0){
+ res=s1[col-1]+res;
+ row-=1;
+ col-=1;
+}
+ return res;
+};
+console.log(findCommon(str1,str2));
